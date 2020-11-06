22f9e0e7f5e2ac868cf15d7065498ca89fdfd387
xiaowen <846703636@qq.com>
2020-11-06 14:12:52 +0800

    finish homework

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.util.AgeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +10,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PersonVo {
 
-  private String id;
-  private Integer age;
-  private String name;
-  private String hobby;
+    private String id;
+    @JsonSerialize(nullsUsing = AgeSerializer.class)
+    private Integer age;
+    private String name;
+    private String hobby;
 }
